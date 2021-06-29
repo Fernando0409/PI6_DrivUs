f6e71ceda11ccdaef9242941d307fb18d9c4e003
Mario H. Morales <mmorales13@ucol.mx>
2021-06-29 15:08:19 -0500

    Applicacion Movil

---
+++ DrivUs/app/src/main/java/com/example/drivus/LoginActivity.java
@@ -0,0 +1,104 @@
+package com.example.drivus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    TextInputLayout etLogin_Email, etLogin_Pass;
+    MaterialButton bnLogin, bnSignUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_activity);
+        etLogin_Email = (TextInputLayout) findViewById(R.id.editEmail);
+        etLogin_Pass = (TextInputLayout) findViewById(R.id.editPassword);
+        bnLogin = (MaterialButton) findViewById(R.id.btnLogin);
+        bnSignUp = (MaterialButton) findViewById(R.id.btnSignUp);
+
+        bnLogin.setOnClickListener(this);
+        bnSignUp.setOnClickListener(this);
+    }
+
+    private void validar_usuarios(String URL){
+        String User_Email = etLogin_Email.getEditText().getText().toString().trim();
+        String User_Pass = etLogin_Pass.getEditText().getText().toString().trim();
+
+        if(User_Email.isEmpty()){
+            etLogin_Email.setError("Ingrese Correo");
+        } else if(User_Pass.isEmpty()){
+            etLogin_Pass.setError("Ingrese Contraseña");
+        }else{
+            StringRequest respuesta = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject login = new JSONObject(response);
+                        String emailUser = login.getString("email");
+                        String passwordUser = login.getString("password");
+                        if (emailUser.equals(User_Email) && passwordUser.equals(User_Pass)) {
+                            Intent inicio_activity = new Intent(LoginActivity.this, Inicio_Prueba.class);
+                            startActivity(inicio_activity);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Usuario o Contraseña Incorrecta", Toast.LENGTH_LONG).show();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(LoginActivity.this, error.toString(), Toast.LENGTH_LONG).show();
+                }
+            }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> parametros = new HashMap<String, String>();
+                    parametros.put("usr_correo", User_Email);
+                    parametros.put("usr_password", User_Pass);
+                    return parametros;
+                }
+            };
+
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(respuesta);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btnLogin:
+                validar_usuarios("http://192.168.1.74/drivus_app_php/login.php");
+                break;
+
+            case R.id.btnSignUp:
+                Intent registro_activity = new Intent(LoginActivity.this, Registro_Nombre.class);
+                startActivity(registro_activity);
+                break;
+        }
+    }
+}
---
+++ DrivUs/app/src/main/java/com/example/drivus/elementos_listas_carros.java
@@ -0,0 +1,173 @@
+package com.example.drivus;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+public class elementos_listas_carros {
+
+    public String id;
+    public String nombre;
+    public String marca;
+    public String placas;
+    public String color;
+    public String precio;
+    public String anioo;
+    public String kilometraje;
+    public String combustible;
+    public String cambios;
+    public String status;
+    public String hora;
+    public String fecha;
+    public String dato;
+    public Bitmap imagen;
+    public String rutaImagen;
+
+    public String getDato() {
+        return dato;
+    }
+
+    public void setDato(String dato) {
+        this.dato = dato;
+
+        try {
+            byte[] byteCode= Base64.decode(dato,Base64.DEFAULT);
+            //this.imagen= BitmapFactory.decodeByteArray(byteCode,0,byteCode.length);
+
+            int alto=99;//alto en pixeles
+            int ancho=50;//ancho en pixeles
+
+            Bitmap foto= BitmapFactory.decodeByteArray(byteCode,0,byteCode.length);
+            this.imagen=Bitmap.createScaledBitmap(foto,alto,ancho,true);
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public Bitmap getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(Bitmap imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getRutaImagen() {
+        return rutaImagen;
+    }
+
+    public void setRutaImagen(String rutaImagen) {
+        this.rutaImagen = rutaImagen;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getPlacas() {
+        return placas;
+    }
+
+    public void setPlacas(String placas) {
+        this.placas = placas;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getAnioo() {
+        return anioo;
+    }
+
+    public void setAnioo(String anioo) {
+        this.anioo = anioo;
+    }
+
+    public String getKilometraje() {
+        return kilometraje;
+    }
+
+    public void setKilometraje(String kilometraje) {
+        this.kilometraje = kilometraje;
+    }
+
+    public String getCombustible() {
+        return combustible;
+    }
+
+    public void setCombustible(String combustible) {
+        this.combustible = combustible;
+    }
+
+    public String getCambios() {
+        return cambios;
+    }
+
+    public void setCambios(String cambios) {
+        this.cambios = cambios;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+
+
+
+}
